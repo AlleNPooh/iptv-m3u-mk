1bd27351e0239bba307e762f6c252a7c27497e41
AllenPooh <pdzbyg@gmail.com>
2021-08-24 21:26:32 +0800

    Update dotpy.py

--- python/plugins/dotpy.py
+++ python/plugins/dotpy.py
@@ -22,7 +22,7 @@ class Source (object) :
         self.now = int(time.time() * 1000)
 
     def getSource (self) :
-        sourcePath = os.getcwd()+'python/plugins/dotpy_source'
+        sourcePath = os.getcwd()+'/python/plugins/dotpy_source'
 		#sourcePath = '/srv/iptv/python/plugins/dotpy_source'
         with open(sourcePath, 'r',encoding='utf-8') as f:
             lines = f.readlines()
